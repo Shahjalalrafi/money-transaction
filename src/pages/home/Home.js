f68409bbdce7e12e5f2651c3be20d55e375f66af
Shahjalalrafi <shahjalalrafi5@gmail.com>
2021-12-16 09:34:21 +0600

    home page functionality implemented

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
+// import styles 
+import classes from "./Home.module.css"
+import TransactionForm from './TransactionForm'
+
 export default function Home() {
     return (
-        <div>
-            home
+        <div className={classes.container}>
+            <div className={classes.content}>
+                transaction list
+            </div>
+            <div className={classes.sidebar}>
+                <TransactionForm />
+            </div>
         </div>
     )
 }
