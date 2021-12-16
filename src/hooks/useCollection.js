434ab286c04575c61c0a898ed7e3bdf3ce7ea72c
Shahjalalrafi <shahjalalrafi5@gmail.com>
2021-12-16 23:24:01 +0600

    collection custom hook implemented

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+import { projectFirestore } from "../config/config"
+
+export const useCollection = (collection) => {
+    const [error, setError] = useState(null)
+    const [document, setDocument] = useState([])
+
+    useEffect(() => {
+        const ref = projectFirestore.collection(collection)
+
+        const unSubscribe = ref.onSnapshot((shapshot) => {
+            let result = []
+            shapshot.docs.map(doc => {
+                result.push({ ...doc.data(), id: doc.id })
+            })
+
+            // update Document
+            setDocument(result)
+
+            setError(null)
+        }, (error) => {
+            console.log(error)
+            setError("Could not fetch the data")
+        })
+
+        return () => unSubscribe()
+    }, [collection])
+
+    return {error, }
+
+}
