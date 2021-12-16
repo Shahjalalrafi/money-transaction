ceea65525ed3b6e55683c4b3fb5f51fd0b480eb2
Shahjalalrafi <shahjalalrafi5@gmail.com>
2021-12-16 09:05:30 +0600

    conditionaly navigate

--- src/App.js
+++ src/App.js
@@ -4,19 +4,39 @@ import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 import Navbar from './components/Navbar/Navbar';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate  } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const { authIsReady, user } = useAuthContext()
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </BrowserRouter>
+      {
+        authIsReady && <BrowserRouter>
+          <Navbar />
+          <Routes>
+            {
+              user && <Route path="/" element={<Home />} />
+            }
+            {
+              !user && <Route path="/" element={<Navigate replace to="/login" />}/>
+            }
+            {
+              !user && <Route path="/login" element={<Login />} />
+            }
+            {
+              user && <Route path="/login" element={<Navigate replace to = "/" />} />
+            }
+            {
+              !user && <Route path="/signup" element={<Signup />} />
+            }
+            {
+              user && <Route path="/signup" element={<Navigate replace to = "/" />} />
+            }
+            {/* <Route path="/signup" element={<Signup />} /> */}
+          </Routes>
+        </BrowserRouter>
+      }
     </div>
   );
 }
