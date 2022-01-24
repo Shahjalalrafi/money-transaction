b384e3aec8f85a7a6c38ddddbe9265798d59c9ec
Shahjalalrafi <shahjalalrafi5@gmail.com>
2022-01-24 13:17:14 +0600

    using our custom hook to delete our document

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { useFirestore } from '../../hooks/useFirestore'
 import classes from "./Home.module.css"
 
-export default function TransactionList({transactions}) {
+export default function TransactionList({ transactions }) {
+    const { deleteDocument } = useFirestore("transactions")
     return (
         <ul className={classes.transactions}>
             {transactions.map((transaction) => (
                 <li key={transaction.id}>
                     <p className={classes.name}>{transaction.name}</p>
                     <p className={classes.amount}>${transaction.amount}</p>
+                    <button onClick={() => deleteDocument(transaction.id)}>X</button>
                 </li>
             ))}
         </ul>
